103685a51cfbee547bc2825a7e1d1860e0442d8c
cajax <cajax1@gmail.com>
2025-04-16 17:17:34 +0200

    adding debug plcaeholder

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-module.exports = {
-    rules: {
-        'prefer-jest-when': require('./lib/rules/prefer-jest-when'),
-        'prefer-once-mock-methods': require('./lib/rules/prefer-once-mock-methods'),
-        'verify-when-mocks-in-after-each': require('./lib/rules/verify-when-mocks-in-after-each'),
-        'no-reset-when-mocks-in-after-each': require('./lib/rules/no-reset-when-mocks-in-after-each'),
-        'no-reset-when-mocks-at-end': require('./lib/rules/no-reset-when-mocks-at-end')
-    },
-    configs: {
-        recommended: {
-            plugins: ['jest-when'],
-            rules: {
-                'jest-when/prefer-jest-when': 'error',
-                'jest-when/prefer-once-mock-methods': 'error',
-                'jest-when/verify-when-mocks-in-after-each': 'error',
-                'jest-when/no-reset-when-mocks-in-after-each': 'error',
-                'jest-when/no-reset-when-mocks-at-end': 'error'
-            }
-        }
+{
+    "name": "eslint-plugin-jest-when",
+    "version": "0.0.3",
+    "description": "ESLint plugin for enforcing best practices with jest-when. Made with Claude AI for personal use.",
+    "main": "index.js",
+    "keywords": [
+      "eslint",
+      "eslintplugin",
+      "eslint-plugin",
+      "jest",
+      "jest-when"
+    ],
+    "author": "cajax",
+    "license": "MIT",
+    "git-repository": "https://github.com/cajax/eslint-plugin-jest-when",
+    "peerDependencies": {
+      "eslint": "^8.0.0"
     }
-};
+  }
